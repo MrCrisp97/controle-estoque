5a754cbbc4b7fd247d2d75f2a924ab540dfac624
MrCrisp <cristianbpivoto15@gmail.com>
2024-06-24 15:16:25 -0300

    tentativa de publicação no railway

--- src/main/java/me/crisp/model/Produto.java
+++ src/main/java/me/crisp/model/Produto.java
@@ -5,7 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-@Entity
+@Entity(name = "tb_produto")
 public class Produto {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
